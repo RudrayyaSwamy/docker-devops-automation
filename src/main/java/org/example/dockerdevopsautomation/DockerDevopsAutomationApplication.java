9018935c474b5ee70375c136056b5c25a7032917
RudrayyaSwamu <rudrayyaswamy248@gmail.com>
2024-02-10 10:28:14 +0530

    first commit

---
+++ src/main/java/org/example/dockerdevopsautomation/DockerDevopsAutomationApplication.java
@@ -0,0 +1,13 @@
+package org.example.dockerdevopsautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDevopsAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDevopsAutomationApplication.class, args);
+	}
+
+}
