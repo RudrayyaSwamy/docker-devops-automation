3663459950520a0d98dc5fd07b7c836860ed66f7
RudrayyaSwamu <rudrayyaswamy248@gmail.com>
2024-02-10 10:35:52 +0530

    changes

--- src/main/java/org/example/dockerdevopsautomation/DockerDevopsAutomationApplication.java
+++ src/main/java/org/example/dockerdevopsautomation/DockerDevopsAutomationApplication.java
@@ -2,12 +2,23 @@ package org.example.dockerdevopsautomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class DockerDevopsAutomationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerDevopsAutomationApplication.class, args);
-	}
+
+    @GetMapping("/")
+    public String getMessage(){
+        return "welcome to docker devops automation";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDevopsAutomationApplication.class, args);
+    }
 
 }
